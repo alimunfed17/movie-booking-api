0890f0b8265311e57cfb753f022527eaf820fd90
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-10-06 01:14:55 -0700

    feat(models): Add Movie, Show, Booking and admin registration

--- moviebooking/admin.py
+++ moviebooking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'duration_minutes')
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'screen_name', 'date_time', 'total_seats')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'show', 'seat_number', 'status', 'created_at')
